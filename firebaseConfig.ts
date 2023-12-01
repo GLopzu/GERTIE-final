01d5c8d57ac9609b710641d5f6c4ca41438b3d95
LOPZU <116040988+GLopzu@users.noreply.github.com>
2023-12-01 17:32:29 -0500

    first commit

---
+++ firebaseConfig.ts
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyBEYC2DHtjPNZJkc9h54iOPWV-HcfPHPco",
+    authDomain: "gertie-tv-animation.firebaseapp.com",
+    projectId: "gertie-tv-animation",
+    storageBucket: "gertie-tv-animation.appspot.com",
+    messagingSenderId: "830001652584",
+    appId: "1:830001652584:web:e1be5ce85f6ac0e85becdd"  
+};
